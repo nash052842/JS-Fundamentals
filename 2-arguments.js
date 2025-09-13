2949559d451e9034ac11ec799d229fb004ec9979
nmwaura4 <nmwaura4@gmail.com>
2025-09-13 23:58:41 +0300

    third commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,11 @@
 // 2-arguments.js
+// 2-arguments.js
 
 if (process.argv.length <= 2) {
   console.log("No argument");
-} else {
+} else if (process.argv.length === 3) {
   console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
+
