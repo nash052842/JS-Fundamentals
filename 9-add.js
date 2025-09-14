9b2470af8f40e408105196aac13dcba16715efe8
nmwaura4 <nmwaura4@gmail.com>
2025-09-14 16:17:00 +0300

    9-add.js

---
+++ 9-add.js
@@ -0,0 +1,10 @@
+const args = process.argv;
+
+function add(a, b) {
+    return a + b;
+}
+
+const num1 = parseInt(args[2], 10);
+const num2 = parseInt(args[3], 10);
+
+console.log(add(num1, num2));
